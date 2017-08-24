b16992ca59d45e40e3670b94d3c30f519484f0f1
brankomilicevic <branko.milicevic@hotmail.com>
2017-08-24 14:52:41 +0200

    first

--- app/src/main/java/si/greyhat/skylink/MainActivity.java
+++ app/src/main/java/si/greyhat/skylink/MainActivity.java
@@ -1,20 +1,29 @@
 package si.greyhat.skylink;
 
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import org.spongycastle.jce.provider.BouncyCastleProvider;
 
+import java.io.IOException;
 import java.security.Security;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements SurfaceHolder.Callback {
+
+    private MediaPlayer mp = null;
+    private SurfaceView mSurfaceView = null;
 
     String TAG = "skylink";
 
@@ -33,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        releaseMediaPlayer();
         Log.i(TAG, "onPause");
     }
 
@@ -51,7 +61,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        releaseMediaPlayer();
         Log.i(TAG, "onDestroy");
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
     }
 
     @Override
@@ -71,17 +90,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mSurfaceView = (SurfaceView)findViewById(R.id.surfaceView);
+        mSurfaceView.getHolder().addCallback(this);
+        mp = new MediaPlayer();
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+
+        AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.slideshow);
+
+        try {
+            mp.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getDeclaredLength());
+            mp.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Get the dimensions of the video
+        int videoWidth = mp.getVideoWidth();
+        int videoHeight = mp.getVideoHeight();
+
+        //Get the width of the screen
+        int screenWidth = getWindowManager().getDefaultDisplay().getWidth();
+
+        //Get the SurfaceView layout parameters
+        android.view.ViewGroup.LayoutParams lp = mSurfaceView.getLayoutParams();
+
+        //Set the width of the SurfaceView to the width of the screen
+        lp.width = screenWidth;
+
+        //Set the height of the SurfaceView to match the aspect ratio of the video
+        //be sure to cast these as floats otherwise the calculation will likely be 0
+        lp.height = (int) (((float)videoHeight / (float)videoWidth) * (float)screenWidth);
+
+        //Commit the layout parameters
+        mSurfaceView.setLayoutParams(lp);
+
+        //Start video
+        mp.setDisplay(holder);
+        mp.setLooping(true);
+        mp.start();
     }
 
     @Override
@@ -106,10 +157,11 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
-    static {
-        Security.addProvider(new BouncyCastleProvider());
+    private void releaseMediaPlayer() {
+        if (mp != null) {
+            mp.release();
+            mp = null;
+        }
     }
-
 
 }
